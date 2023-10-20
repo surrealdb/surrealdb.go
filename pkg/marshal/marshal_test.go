3f9fcf014b288d17f33809bdf7279e4ccec16fd0
ElecTwix <51170038+ElecTwix@users.noreply.github.com>
2023-10-20 15:51:06 +0300

    TEST: add some parts of UnmarshalMapToStruct unit test

--- pkg/marshal/marshal_test.go
+++ pkg/marshal/marshal_test.go
@@ -1,1 +1,29 @@
 package marshal_test
+
+import (
+	"testing"
+
+	"github.com/surrealdb/surrealdb.go/pkg/marshal"
+)
+
+type testMarshalStruct struct {
+	TestInt  int  `json:"TestInt"`
+	TestBool bool `json:"TestBool"`
+}
+
+func TestUnMarshalMapToStruct(t *testing.T) {
+	testDataMap := make(map[string]interface{}, 2)
+
+	testObj := &testMarshalStruct{
+		TestInt:  15,
+		TestBool: true,
+	}
+
+	testDataMap["TestInt"] = testObj.TestInt
+	testDataMap["TestBool"] = testObj.TestBool
+
+	err := marshal.UnmarshalMapToStruct(testDataMap, testObj)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
