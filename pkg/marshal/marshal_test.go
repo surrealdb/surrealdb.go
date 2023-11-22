3ab54154218dfce604ac3fe792c52c29aa94837b
ElecTwix <51170038+ElecTwix@users.noreply.github.com>
2023-11-22 21:23:18 +0300

    move mapToStruct to marshal package added unit tests

---
+++ pkg/marshal/marshal_test.go
@@ -0,0 +1,28 @@
+package marshal_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/surrealdb/surrealdb.go/pkg/marshal"
+)
+
+type testMarshalStruct struct {
+	TestInt  int  `json:"TestInt"`
+	TestBool bool `json:"TestBool"`
+}
+
+func TestUnMarshalMapToStruct(t *testing.T) {
+	testDataMap := make(map[string]interface{}, 2)
+
+	testObj := &testMarshalStruct{
+		TestInt:  15,
+		TestBool: true,
+	}
+
+	testDataMap["TestInt"] = testObj.TestInt
+	testDataMap["TestBool"] = testObj.TestBool
+
+	err := marshal.UnmarshalMapToStruct(testDataMap, testObj)
+	require.NoError(t, err)
+}
