cb1efe1cab470ac6f03ca29fd3718ceae2c8d85d
Yuske Kuoka <yusuke.kuoka@surrealdb.com>
2025-07-15 04:21:46 +0000

    Remove redundant second unmarshalling of GeometryPoint

--- pkg/models/geometry.go
+++ pkg/models/geometry.go
@@ -38,14 +38,23 @@ func (gp *GeometryPoint) UnmarshalCBOR(data []byte) error {
 		return fmt.Errorf("unexpected tag number: got %d, want %d", tag.Number, TagGeometryPoint)
 	}
 
-	var temp [2]float64
-	err := cbor.Unmarshal(data, &temp)
-	if err != nil {
-		return err
+	content, ok := tag.Content.([]any)
+	if !ok {
+		return fmt.Errorf("unexpected content type: got %T, want [2]float64", tag.Content)
 	}
 
-	gp.Latitude = temp[0]
-	gp.Longitude = temp[1]
+	lat, ok := content[0].(float64)
+	if !ok {
+		return fmt.Errorf("unexpected type for latitude: got %T, want float64", content[0])
+	}
+
+	lon, ok := content[1].(float64)
+	if !ok {
+		return fmt.Errorf("unexpected type for longitude: got %T, want float64", content[1])
+	}
+
+	gp.Latitude = lat
+	gp.Longitude = lon
 
 	return nil
 }
