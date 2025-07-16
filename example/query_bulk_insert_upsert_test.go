9752e46271dfdf665fd7d0e864baec96b2a12904
Yuske Kuoka <yusuke.kuoka@surrealdb.com>
2025-07-16 12:18:40 +0000

    Add testable example for bulk inserts and upserts
    Ref #224

---
+++ example/query_bulk_insert_upsert_test.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	surrealdb "github.com/surrealdb/surrealdb.go"
+	"github.com/surrealdb/surrealdb.go/pkg/models"
+)
+
+// This example demonstrates how you can batch insert and upsert records,
+// with specifying RETURN NONE to avoid unnecessary data transfer and decoding.
+//
+//nolint:funlen
+func ExampleQuery_bluk_insert() {
+	db := newSurrealDBWSConnection("query", "persons")
+
+	type Person struct {
+		ID   *models.RecordID `json:"id,omitempty"`
+		Note string           `json:"note,omitempty"`
+	}
+
+	nthPerson := func(i int) Person {
+		return Person{
+			ID:   &models.RecordID{Table: "persons", ID: fmt.Sprintf("p%d", i)},
+			Note: fmt.Sprintf("inserted%d", i),
+		}
+	}
+
+	var persons []Person
+	for i := 0; i < 2; i++ {
+		persons = append(persons, nthPerson(i))
+	}
+
+	insert, err := surrealdb.Query[any](
+		db,
+		`INSERT INTO persons $persons RETURN NONE`,
+		map[string]any{
+			"persons": persons,
+		})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("# INSERT INTO")
+	fmt.Printf("Count   : %d\n", len(*insert))
+	fmt.Printf("Status  : %+s\n", (*insert)[0].Status)
+	fmt.Printf("Result  : %+v\n", (*insert)[0].Result)
+
+	select1, err := surrealdb.Query[[]Person](
+		db,
+		`SELECT * FROM persons ORDER BY id.id`,
+		nil)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Selected: %+v\n", (*select1)[0].Result)
+
+	persons = append(persons, nthPerson(2))
+
+	insertIgnore, err := surrealdb.Query[any](
+		db,
+		`INSERT IGNORE INTO persons $persons RETURN NONE`,
+		map[string]any{
+			"persons": persons,
+		})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("# INSERT IGNORE INTO")
+	fmt.Printf("Count   : %d\n", len(*insertIgnore))
+	fmt.Printf("Status  : %+s\n", (*insertIgnore)[0].Status)
+	fmt.Printf("Result  : %+v\n", (*insertIgnore)[0].Result)
+
+	select2, err := surrealdb.Query[[]Person](
+		db,
+		`SELECT * FROM persons ORDER BY id.id`,
+		nil)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Selected: %+v\n", (*select2)[0].Result)
+
+	for i := 0; i < 3; i++ {
+		persons[i].Note = fmt.Sprintf("updated%d", i)
+	}
+	persons = append(persons, nthPerson(3))
+	var upsertQueries []string
+	vars := make(map[string]any)
+	for i, p := range persons {
+		upsertQueries = append(upsertQueries,
+			fmt.Sprintf(`UPSERT persons CONTENT $content%d RETURN NONE`, i),
+		)
+		vars[fmt.Sprintf("content%d", i)] = p
+	}
+	upsert, err := surrealdb.Query[any](
+		db,
+		strings.Join(upsertQueries, ";"),
+		vars,
+	)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("# UPSERT CONTENT")
+	fmt.Printf("Count   : %d\n", len(*upsert))
+	fmt.Printf("Status  : %+s\n", (*upsert)[0].Status)
+	fmt.Printf("Result  : %+v\n", (*upsert)[0].Result)
+
+	select3, err := surrealdb.Query[[]Person](
+		db,
+		`SELECT * FROM persons ORDER BY id.id`,
+		nil)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Selected: %+v\n", (*select3)[0].Result)
+
+	//nolint:lll
+	// Output:
+	// # INSERT INTO
+	// Count   : 1
+	// Status  : OK
+	// Result  : []
+	// Selected: [{ID:persons:p0 Note:inserted0} {ID:persons:p1 Note:inserted1}]
+	// # INSERT IGNORE INTO
+	// Count   : 1
+	// Status  : OK
+	// Result  : []
+	// Selected: [{ID:persons:p0 Note:inserted0} {ID:persons:p1 Note:inserted1} {ID:persons:p2 Note:inserted2}]
+	// # UPSERT CONTENT
+	// Count   : 4
+	// Status  : OK
+	// Result  : []
+	// Selected: [{ID:persons:p0 Note:updated0} {ID:persons:p1 Note:updated1} {ID:persons:p2 Note:updated2} {ID:persons:p3 Note:inserted3}]
+}
