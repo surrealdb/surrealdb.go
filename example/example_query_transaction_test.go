f900f1d7b7dde0d8856e88814abfbaad4a519af9
Yuske Kuoka <yusuke.kuoka@surrealdb.com>
2025-07-19 12:17:47 +0000

    Prevent making query error look like unmarshal error
    Fixes #175

--- example/example_query_transaction_test.go
+++ example/example_query_transaction_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 
 	surrealdb "github.com/surrealdb/surrealdb.go"
@@ -31,24 +32,69 @@ func ExampleQuery_transaction_return() {
 func ExampleQuery_transaction_throw() {
 	db := newSurrealDBWSConnection("query", "person")
 
-	var err error
+	var (
+		queryResults *[]surrealdb.QueryResult[*int]
+		err          error
+	)
 
-	// Making b parameterized with `bool` type
-	// would make it fail with `cannot unmarshal UTF-8 text string into Go struct field`
-	// like reported in https://github.com/surrealdb/surrealdb.go/issues/175
-	var b *[]surrealdb.QueryResult[any]
-	b, err = surrealdb.Query[any](
+	// Up until v0.4.3, making QueryResult[T] parameterized with anything other than `any`
+	// or `string` failed with:
+	//   cannot unmarshal UTF-8 text string into Go struct field
+	// in case the query was executed on the database but failed with an error.
+	//
+	// It was due to a mismatch between the expected type and the actual type-
+	// The actual query result was a string, which provides the error message sent
+	// from the database, regardless of the type parameter specified to the Query function.
+	//
+	// Since v0.4.4, the QueryResult was enhanced to set the Error field
+	// to a QueryError if the query failed, allowing the caller to handle the error.
+	// In that case, the Result field will be empty(or nil if it is a pointer type),
+	// and the Status field will be set to "ERR".
+	//
+	// It's also worth noting that the returned error from the Query function
+	// will be nil if the query was executed successfully, in which case all the results
+	// have no Error field set.
+	//
+	// If the query failed, the returned error will be a `joinError` created by the `errors.Join` function,
+	// which contains all the errors that occurred during the query execution.
+	// The caller can check the Error field of each QueryResult to see if the query failed,
+	// or check the returned error from the Query function to see if the query failed.
+	queryResults, err = surrealdb.Query[*int](
 		db,
 		`BEGIN; THROW "test"; RETURN 1; COMMIT;`,
 		nil,
 	)
-	if err != nil {
-		panic(err)
+	fmt.Printf("# of results: %d\n", len(*queryResults))
+	fmt.Println("=== Func error ===")
+	fmt.Printf("Error: %v\n", err)
+	fmt.Printf("Error is RPCError: %v\n", errors.Is(err, &surrealdb.RPCError{}))
+	fmt.Printf("Error is QueryError: %v\n", errors.Is(err, &surrealdb.QueryError{}))
+	for i, r := range *queryResults {
+		fmt.Printf("=== QueryResult[%d] ===\n", i)
+		fmt.Printf("Status: %v\n", r.Status)
+		fmt.Printf("Result: %v\n", r.Result)
+		fmt.Printf("Error: %v\n", r.Error)
+		fmt.Printf("Error is RPCError: %v\n", errors.Is(r.Error, &surrealdb.RPCError{}))
+		fmt.Printf("Error is QueryError: %v\n", errors.Is(r.Error, &surrealdb.QueryError{}))
 	}
-	fmt.Printf("Status: %v\n", (*b)[0].Status)
-	fmt.Printf("Result: %v\n", (*b)[0].Result)
 
 	// Output:
+	// # of results: 2
+	// === Func error ===
+	// Error: An error occurred: test
+	// The query was not executed due to a failed transaction
+	// Error is RPCError: false
+	// Error is QueryError: true
+	// === QueryResult[0] ===
 	// Status: ERR
-	// Result: An error occurred: test
+	// Result: <nil>
+	// Error: An error occurred: test
+	// Error is RPCError: false
+	// Error is QueryError: true
+	// === QueryResult[1] ===
+	// Status: ERR
+	// Result: <nil>
+	// Error: The query was not executed due to a failed transaction
+	// Error is RPCError: false
+	// Error is QueryError: true
 }
--- example/example_upsert_test.go
+++ example/example_upsert_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"time"
 
@@ -99,4 +100,80 @@ func ExampleUpsert() {
 	// Update via upsert result: {persons:yusuke Yusuke Updated {0001-01-01 00:00:00 +0000 UTC} 2023-10-01T12:00:00Z}
 	// Update further via upsert result: {persons:yusuke Yusuke Updated Further {2023-10-01 12:00:00 +0000 UTC} 2023-10-02T12:00:00Z}
 	// Selected person: {persons:yusuke Yusuke Updated Last {0001-01-01 00:00:00 +0000 UTC} <nil>}
+}
+
+func ExampleUpsert_unmarshal_error() {
+	db := newSurrealDBWSConnection("query", "person")
+
+	type Person struct {
+		Name string `json:"name"`
+	}
+
+	// This will fail because the record ID is not valid.
+	_, err := surrealdb.Upsert[Person](
+		db,
+		models.Table("person"),
+		map[string]any{
+			// We are trying to set the name field to a number,
+			// which is OK from the database's perspective,
+			// because the table is schemaless for this example.
+			//
+			// However, we are trying to unmarshal the result into a struct
+			// that expects the name field to be a string,
+			// which will fail when the result is unmarshaled.
+			"name": 123,
+		},
+	)
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		fmt.Printf("Error is RPCError: %v\n", errors.Is(err, &surrealdb.RPCError{}))
+	}
+
+	// Output:
+	// Error: error unmarshaing response: Send: error unmarshaling result: cbor: cannot unmarshal array into Go value of type main.Person (cannot decode CBOR array to struct without toarray option)
+	// Error is RPCError: false
+}
+
+func ExampleUpsert_rpc_error() {
+	db := newSurrealDBWSConnection("query", "person")
+
+	type Person struct {
+		Name string `json:"name"`
+	}
+
+	// For this example, we will define a SCHEMAFUL table
+	// with a name field that is a string.
+	// Trying to set the name field to a number
+	// will result in an error from the database.
+
+	if _, err := surrealdb.Query[any](
+		db,
+		`DEFINE TABLE person SCHEMAFUL;
+		 DEFINE FIELD name ON person TYPE string;`,
+		nil,
+	); err != nil {
+		panic(err)
+	}
+
+	// This will fail because the record ID is not valid.
+	_, err := surrealdb.Upsert[Person](
+		db,
+		models.Table("person"),
+		map[string]any{
+			"id": models.NewRecordID("person", "a"),
+			// Unlike ExampleUpsert_unmarshal_error,
+			// this will fail on the database side
+			// because the name field is defined as a string,
+			// and we are trying to set it to a number.
+			"name": 123,
+		},
+	)
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		fmt.Printf("Error is RPCError: %v\n", errors.Is(err, &surrealdb.RPCError{}))
+	}
+
+	// Output:
+	// Error: There was a problem with the database: Couldn't coerce value for field `name` of `person:a`: Expected `string` but found `123`
+	// Error is RPCError: true
 }
