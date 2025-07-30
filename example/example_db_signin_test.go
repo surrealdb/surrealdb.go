e2813e9fe7581686d9c603936b9a2658acc16f3b
Yuske Kuoka <yusuke.kuoka@surrealdb.com>
2025-07-30 08:10:34 +0000

    Add SignIn example

---
+++ example/example_db_signin_test.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	surrealdb "github.com/surrealdb/surrealdb.go"
+)
+
+func ExampleDB_signin_failure() {
+	db, err := surrealdb.Connect(
+		context.Background(),
+		getSurrealDBWSURL(),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	// Attempt to sign in without setting namespace or database
+	// This should fail with an error, whose message will depend on the connection type.
+	_, err = db.SignIn(context.Background(), surrealdb.Auth{
+		Username: "root",
+		Password: "invalid",
+	})
+	switch err.Error() {
+	case "namespace or database or both are not set":
+		// In case the connection is over HTTP, this error is expected
+	case "There was a problem with the database: There was a problem with authentication":
+		// In case the connection is over WebSocket, this error is expected
+	default:
+		panic(fmt.Sprintf("Unexpected error: %v", err))
+	}
+
+	err = db.Use(context.Background(), "testNS", "testDB")
+	if err != nil {
+		fmt.Println("Use error:", err)
+	}
+
+	// Even though the ns/db is set, the SignIn should still fail
+	// when the credentials are invalid.
+	_, err = db.SignIn(context.Background(), surrealdb.Auth{
+		Username: "root",
+		Password: "invalid",
+	})
+	fmt.Println("SignIn error:", err)
+
+	// Now let's try with the correct credentials
+	// This should succeed if the database is set up correctly.
+	_, err = db.SignIn(context.Background(), surrealdb.Auth{
+		Username: "root",
+		Password: "root",
+	})
+	if err != nil {
+		panic(fmt.Sprintf("SignIn failed: %v", err))
+	}
+
+	// Output:
+	// SignIn error: There was a problem with the database: There was a problem with authentication
+}
