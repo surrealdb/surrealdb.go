777d4a3a6d0106f14589246b163e52752d1732c2
Yuske Kuoka <yusuke.kuoka@surrealdb.com>
2025-08-13 05:42:17 +0000

    Prevent allocation of huge cbor array

--- surrealcbor/decode_array.go
+++ surrealcbor/decode_array.go
@@ -3,9 +3,16 @@ package surrealcbor
 import (
 	"fmt"
 	"io"
-	"math"
 	"reflect"
 )
+
+// MaxCBORArrayLength is used to enforce a maximum
+// allowed array length when decoding CBOR arrays
+//
+// This originates from the following CodeQL finding:
+//
+//	Slice memory allocation with excessive size value
+var MaxCBORArrayLength uint64 = 1000000
 
 // decodeArray decodes a CBOR array (Major Type 4) into the given reflect.Value.
 // https://www.rfc-editor.org/rfc/rfc8949.html#section-3.1-2.10
@@ -19,34 +26,32 @@ func (d *decoder) decodeArray(v reflect.Value) error {
 		return d.decodeIndefiniteArray(v)
 	}
 
-	length, err := d.readUint()
+	var length int
+	err := d.readLength(&length)
 	if err != nil {
 		return err
 	}
 
 	switch v.Kind() {
 	case reflect.Slice:
-		v.Set(reflect.MakeSlice(v.Type(), int(length), int(length)))
-		for i := 0; i < int(length); i++ {
+		v.Set(reflect.MakeSlice(v.Type(), length, length))
+		for i := 0; i < length; i++ {
 			if err := d.decodeValue(v.Index(i)); err != nil {
 				return err
 			}
 		}
 	case reflect.Array:
-		if length > math.MaxInt64 {
-			return fmt.Errorf("array length %d overflows int", length)
-		}
-		if v.Len() < int(length) {
+		if v.Len() < length {
 			return fmt.Errorf("array too small")
 		}
-		for i := 0; i < int(length); i++ {
+		for i := 0; i < length; i++ {
 			if err := d.decodeValue(v.Index(i)); err != nil {
 				return err
 			}
 		}
 	case reflect.Interface:
 		arr := make([]any, length)
-		for i := 0; i < int(length); i++ {
+		for i := 0; i < length; i++ {
 			var elem any
 			if err := d.decodeValue(reflect.ValueOf(&elem).Elem()); err != nil {
 				return err
@@ -57,6 +62,22 @@ func (d *decoder) decodeArray(v reflect.Value) error {
 	default:
 		return fmt.Errorf("cannot decode array into %v", v.Type())
 	}
+	return nil
+}
+
+func (d *decoder) readLength(dst *int) error {
+	length, err := d.readUint()
+	if err != nil {
+		return err
+	}
+	if length > MaxCBORArrayLength {
+		return fmt.Errorf("CBOR array length %d exceeds maximum allowed (%d)", length, MaxCBORArrayLength)
+	}
+	if length > uint64(int(^uint(0)>>1)) { // check for int overflow
+		return fmt.Errorf("CBOR array length %d overflows int", length)
+	}
+
+	*dst = int(length)
 	return nil
 }
 
