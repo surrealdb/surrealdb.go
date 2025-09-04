df064e051cbe2a050c245fb86a9d79f1870f7e42
Yuske Kuoka <yusuke.kuoka@surrealdb.com>
2025-09-04 09:19:34 +0000

    example: Query with SELECT ONLY

--- example_query_test.go
+++ example_query_test.go
@@ -61,3 +61,54 @@ func ExampleQuery() {
 	// First query result's status: OK
 	// Persons contained in the first query result: [{ID:persons:yusuke Name:Yusuke NestedMap:map[] NestedStruct:{City:Tokyo} CreatedAt:{Time:2023-10-01 12:00:00 +0000 UTC} UpdatedAt:<nil>}]
 }
+
+// The Query function's result type parameter should be varied according to the query.
+// For example, SELECT ONLY returns a single record, not an array of records, and therefore
+// the result type parameter should be a single type, not a slice type.
+//
+//nolint:staticcheck // for demonstration purpose
+func ExampleQuery_only() {
+	db := testenv.MustNew("surrealdbexamples", "query_only", "persons")
+
+	type Person struct {
+		ID   *models.RecordID `json:"id,omitempty"`
+		Name string           `json:"name"`
+	}
+
+	recordID := models.NewRecordID("persons", "yusuke")
+
+	// Note the type parameter is []Person
+	createQueryResults, err := surrealdb.Query[[]Person](
+		context.Background(),
+		db,
+		`CREATE $record_id CONTENT {name: "Yusuke"}`,
+		map[string]any{
+			"record_id": recordID,
+		},
+	)
+	if err != nil {
+		panic(err)
+	}
+	var persons []Person = (*createQueryResults)[0].Result
+	fmt.Printf("Persons contained in the first query result: %+v\n", persons)
+
+	// Note the type parameter is Person, not []Person,
+	// due to the ONLY keyword
+	queryOnlyResults, err := surrealdb.Query[Person](
+		context.Background(),
+		db,
+		`SELECT * FROM ONLY $record_id`,
+		map[string]any{
+			"record_id": recordID,
+		},
+	)
+	if err != nil {
+		panic(err)
+	}
+	var person Person = (*queryOnlyResults)[0].Result
+	fmt.Printf("Person contained in the query only result: %+v\n", person)
+
+	// Output:
+	// Persons contained in the first query result: [{ID:persons:yusuke Name:Yusuke}]
+	// Person contained in the query only result: {ID:persons:yusuke Name:Yusuke}
+}
