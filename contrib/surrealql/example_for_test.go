aae382b8e673583b62e652e48519e0e645015148
Yusuke Kuoka <yusuke.kuoka@surrealdb.com>
2025-10-06 11:21:37 +0900

    surrealql: For + sub-query example (#357)

--- contrib/surrealql/example_for_test.go
+++ contrib/surrealql/example_for_test.go
@@ -30,3 +30,27 @@ func ExampleForStatement_goSliceAsIterable() {
 	// };
 	// Var param_1: [Tobie Jaime]
 }
+
+func ExampleForStatement_subqueryAsIterable() {
+	subquery := surrealql.Select("person").Value("id").Where("age >= ?", 18)
+
+	createUser := surrealql.Update("$person").Set("can_vote = true")
+
+	statement := surrealql.For("person", subquery).
+		Query(createUser)
+
+	sql, vars := statement.Build()
+	fmt.Println(sql)
+
+	keys := sort.StringSlice(slices.Collect(maps.Keys(vars)))
+	sort.Stable(keys)
+	for _, key := range keys {
+		fmt.Printf("Var %s: %v\n", key, vars[key])
+	}
+
+	// Output:
+	// FOR $person IN (SELECT VALUE id FROM person WHERE age >= $param_1) {
+	// UPDATE $person SET can_vote = true;
+	// };
+	// Var param_1: 18
+}
