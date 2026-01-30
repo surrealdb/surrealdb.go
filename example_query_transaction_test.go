d69b03aea95ff741b2687e47879dc9ed4cfbcd77
Yuske Kuoka <yusuke.kuoka@surrealdb.com>
2026-01-30 11:51:51 +0000

    Fix ExampleQuery_transaction_throw for v3

--- example_query_transaction_test.go
+++ example_query_transaction_test.go
@@ -91,12 +91,13 @@ func ExampleQuery_transaction_throw() {
 
 	// Normalize error messages for version compatibility
 	// v2.x: "failed transaction"
-	// v3.x: "canceled transaction"
+	// v3.x: "cancelled transaction" (British spelling)
 	normalizeTransactionError := func(err error) string {
 		if err == nil {
 			return "<nil>"
 		}
 		s := err.Error()
+		s = strings.ReplaceAll(s, "cancelled transaction", "failed transaction")
 		s = strings.ReplaceAll(s, "canceled transaction", "failed transaction")
 		return s
 	}
